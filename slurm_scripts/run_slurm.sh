5a5744fb0540909a44bbf664416adad610703965
jlingford <james.lingford@gmail.com>
2024-11-25 14:09:29 +1100

    update mutation file and slurm script

--- slurm_scripts/run_slurm.sh
+++ slurm_scripts/run_slurm.sh
@@ -17,14 +17,12 @@
 # set env
 module purge
 module load miniforge3
-module load gcc/10.2.0
-module load openmpi/4.1.2-ucx
-conda activate /fs04/scratch2/rp24/jamesl2/RosettaDDGPrediction/rp24_scratch2/jamesl2/miniconda/conda/envs/ddg_rosetta
+conda activate /fs04/scratch2/rp24/jamesl2/ddg_rosetta/rp24_scratch2/jamesl2/miniconda/conda/envs/ddg_rosetta
 
 # set variables
-ROSETTA_DIR=/fs04/scratch2/rp24/jamesl2/RosettaDDGPrediction/rp24_scratch2/jamesl2/miniconda/conda/envs/ddg_rosetta/bin
-CONFIG_RUN=../config/config_run
-CONFIG_SET=../config/config_settings
+ROSETTA_DIR=/fs04/scratch2/rp24/jamesl2/ddg_rosetta/rp24_scratch2/jamesl2/miniconda/conda/envs/ddg_rosetta
+CONFIG_RUN=../RosettaDDGPrediction/config_run
+CONFIG_SET=../RosettaDDGPrediction/config_settings
 MUT_DIR=../muts
 
 # generate help output
@@ -32,10 +30,10 @@ rosetta_ddg_run --help >help.txt
 
 # run rosetta
 rosetta_ddg_run \
-    --pdbfile pdb_input/*.pdb \
-    --listfile $MUT_DIR/mutlist_rosetta.txt \
+    --pdbfile pdb_input/wt_monomer.pdb \
+    --listfile $MUT_DIR/saturation_core_muts.txt \
     --configfile-run $CONFIG_RUN/cartesian2020_ref2015.yaml \
-    --configfile-settings $CONFIG_SET/rosettanompi.yaml \
+    --configfile-settings $CONFIG_SET/rosettampi.yaml \
     --rosettapath $ROSETTA_DIR \
     --saturation
 
