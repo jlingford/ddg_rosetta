6053e163dcf1995c47baf7352b6354d4e9cf492f
jlingford <james.lingford@gmail.com>
2024-11-25 16:45:00 +1100

    update slurm

--- slurm_scripts/short_run_slurm.sh
+++ slurm_scripts/short_run_slurm.sh
@@ -26,6 +26,8 @@ conda activate /fs04/scratch2/rp24/jamesl2/ddg_rosetta/rp24_scratch2/jamesl2/min
 ROSETTA_DIR=/fs04/scratch2/rp24/jamesl2/ddg_rosetta/rp24_scratch2/jamesl2/miniconda/conda/envs/ddg_rosetta
 CONFIG_RUN=RosettaDDGPrediction/config_run
 CONFIG_SET=RosettaDDGPrediction/config_settings
+CONFIG_AGG=RosettaDDGPrediction/config_aggregate
+CONFIG_PLT=RosettaDDGPrediction/config_plot
 MUT_DIR=muts
 
 # generate help output
@@ -40,5 +42,24 @@ rosetta_ddg_run \
     --rosettapath $ROSETTA_DIR \
     -n 48
 
+echo "done rosetta ddg run!"
+
 rosetta_ddg_check_run \
     --configfile-run $CONFIG_RUN/cartesian2020_ref2015.yaml
+
+rosetta_ddg_aggregate \
+    -ca $CONFIG_AGG/aggregate.yaml \
+    -cr $CONFIG_RUN/cartddg_ref2015.yaml \
+    -cs $CONFIG_SET/nompi.yaml \
+    -mf cartesian/mutinfo.txt \
+    -od agg_data \
+    -n 48
+
+echo "done rosetta ddg aggregate!"
+
+rosetta_ddg_plot \
+    -i agg_data/ddg_mutations_structures.csv \
+    -o key_muts \
+    -cp $CONFIG_PLT/total_heatmap.yaml
+
+echo "done rosetta ddg plot!"
