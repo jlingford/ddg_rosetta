88a08adb55a59a189ba01b0ee894e62013321903
jlingford <james.lingford@gmail.com>
2024-11-26 10:39:58 +1100

    tweak local script

--- scripts/local_plot.sh
+++ scripts/local_plot.sh
@@ -3,8 +3,8 @@
 CONFIG_PLT=RosettaDDGPrediction/config_plot
 
 rosetta_ddg_plot \
-    -i agg_data/ddg_mutations_structures.csv \
+    -i data_keymuts1/agg_data/G34R_aggregate.csv \
     -o key_muts \
-    -cp $CONFIG_PLT/total_heatmap.yaml
+    -cp $CONFIG_PLT/contributions_barplot.yaml
 
 echo "done rosetta ddg plot!"
